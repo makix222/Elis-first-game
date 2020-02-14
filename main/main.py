f06ff5446e710f886ec9ec7872c27280a186d33d
Emerson Maki <makix222@gmail.com>
2020-02-14 00:16:36 -0600

    name change

--- main/main.py
+++ main/main.py
@@ -1,6 +1,17 @@
 import pygame
+import main.globals as world_params
 
 
 pygame.init()
 
+size_x = 500
+size_y = 800
+screen_size = size_x, size_y
+
+main_screen = pygame.display.set_mode(screen_size)
+main_clock = pygame.time.Clock()
+
+done = False
+while not done:
+    pygame.Surface().fill(world_params.background_color)
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="totem_tile_game", # Replace with your own username
+    version="0.0.1",
+    author="Emerson Maki",
+    author_email="makix222@gmail.com",
+    description="A simple tile game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
